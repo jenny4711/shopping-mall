c0a9253180e2dd644a5f8dce5a7e8452c823eba9
jiylee <jjenny4711@gmail.com>
2023-10-10 03:30:16 -0400

    Update CloudinaryUploadWidget.js

--- src/utils/CloudinaryUploadWidget.js
+++ src/utils/CloudinaryUploadWidget.js
@@ -5,48 +5,33 @@ const CLOUDNAME = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
 const UPLOADPRESET = process.env.REACT_APP_CLOUDINARY_PRESET;
 
 const CloudinaryUploadWidget = ({ uploadImage }) => {
-  const [uploadedImages, setUploadedImages] = useState([]); 
+  const [uploadedImages, setUploadedImages] = useState([]);
 
-  useEffect(() => {
-    const myWidget = window.cloudinary.createUploadWidget(
-      {
-        cloudName: CLOUDNAME,
-        uploadPreset: UPLOADPRESET,
-        multiple: true, 
-      },
-      (error, result) => {
-        if (!error && result && result.event === "success") {
-          const secureUrl = result.info.secure_url;
-          console.log("이미지 업로드 완료: ", secureUrl);
+  const openWidget = () => {
+    // Upload widget configuration and initialization
+    window.cloudinary
+      .createUploadWidget(
+        {
+          cloudName: CLOUDNAME,
+          uploadPreset: UPLOADPRESET,
+          multiple: true,
+        },
+        (error, result) => {
+          if (!error && result && result.event === "success") {
+            const secureUrl = result.info.secure_url;
+            console.log("이미지 업로드 완료: ", secureUrl);
 
-         
-          setUploadedImages((prevImages) => [...prevImages, secureUrl]);
-          
-      
-          uploadImage(secureUrl);
+            setUploadedImages((prevImages) => [...prevImages, secureUrl]);
+            uploadImage(secureUrl);
+          }
         }
-      }
-    );
-
-    document.getElementById("upload_widget").addEventListener(
-      "click",
-      function () {
-        myWidget.open();
-      },
-      false
-    );
-
-    // 컴포넌트가 언마운트될 때 이벤트 리스너를 정리
-    return () => {
-      document
-        .getElementById("upload_widget")
-        .removeEventListener("click", myWidget.open);
-    };
-  }, [uploadImage]); 
+      )
+      .open();
+  };
 
   return (
     <div>
-      <Button id="upload_widget" size="sm" className="ml-2">
+      <Button onClick={openWidget} size="sm" className="ml-2">
         이미지 업로드 +
       </Button>
       <div>
